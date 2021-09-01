54025cb4e8b68c8f0ea603ae35772532c2511ddf
anbarasinithiya <89622470+anbarasinithiya@users.noreply.github.com>
2021-09-01 08:32:56 +0530

    Add files via upload

---
+++ app/SavedValues.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SavedValues extends Model
+{
+    //
+        protected $fillable = [
+
+        'savedvalue'
+
+    ];
+}
